e4acea36e7037ff90f622ed6d62bbbd8e55b9e66
rajhaga <134823961+rajhaga@users.noreply.github.com>
2025-02-01 19:52:31 +0700

    add tabel

--- app/Models/Mitra.php
+++ app/Models/Mitra.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Mitra extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'nama_laundry',
+        'alamat',
+        'jam_operasional',
+        'layanan',
+        'harga',
+        'metode_pembayaran',
+        'deskripsi',
+        'foto_tempat',
+        'foto_bukti',
+        'lokasi',
+    ];
+
+    // Relasi ke tabel users
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,19 +1,30 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     protected $fillable = [
-        'username', // Pastikan username ada di sini
+        'username',
         'email',
         'password',
         'role',
+        'phone',
     ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // Relasi ke tabel mitras
+    public function mitra()
+    {
+        return $this->hasOne(Mitra::class);
+    }
 }
