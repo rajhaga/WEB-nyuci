ad2b57db461dd8496d7fe29f0e4747b872917fb4
rajhaga <134823961+rajhaga@users.noreply.github.com>
2025-03-18 16:01:03 +0700

    Fix build output directory

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-      tailwindcss(),
+        tailwindcss(),
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
     ],
+    build: {
+        outDir: 'dist', // Menentukan folder output hasil build
+        emptyOutDir: true, // Menghapus isi folder sebelum build baru
+    },
 });
