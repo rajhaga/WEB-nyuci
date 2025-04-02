8dd2781852b0a9f1b3944722e5694d81a5144424
rajhaga <134823961+rajhaga@users.noreply.github.com>
2025-04-02 16:31:33 +0700

    dist update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         tailwindcss(),
+//         laravel([
+//             'resources/css/app.css',
+//             'resources/js/app.js',
+//         ]),
+//     ],
+//     build: {
+//         outDir: 'dist', // Menentukan folder output hasil build
+//         emptyOutDir: true, // Menghapus isi folder sebelum build baru
+//     },
+// });
+
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-    ],
-    build: {
-        outDir: 'dist', // Menentukan folder output hasil build
-        emptyOutDir: true, // Menghapus isi folder sebelum build baru
-    },
-});
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true, // Pastikan ini true
+    outDir: 'public/build', // Lokasi output
+  },
+})
