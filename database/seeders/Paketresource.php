71a5e4dc34e4f7b93ea1fb8a846bc0a652c57d83
rajhaga <134823961+rajhaga@users.noreply.github.com>
2025-04-02 17:37:19 +0700

    seed

--- database/seeders/Paketresource.php
+++ database/seeders/Paketresource.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PaketItemSeeder extends Seeder
+class Paketresource extends Seeder
 {
     /**
      * Run the database seeds.
