2f08d3a7d8507e323c4eb741b0b0533c84d4e687
rajhaga <134823961+rajhaga@users.noreply.github.com>
2025-04-02 18:23:14 +0700

    dist update

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,33 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
-
-// export default defineConfig({
-//     plugins: [
-//         tailwindcss(),
-//         laravel([
-//             'resources/css/app.css',
-//             'resources/js/app.js',
-//         ]),
-//     ],
-//     build: {
-//         outDir: 'dist', // Menentukan folder output hasil build
-//         emptyOutDir: true, // Menghapus isi folder sebelum build baru
-//     },
-// });
-
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  build: {
-    manifest: true, // Pastikan ini true
-    outDir: 'public/build', // Lokasi output
-  },
-})
+    plugins: [
+        tailwindcss(),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+    ],
+    build: {
+        outDir: 'dist', // Menentukan folder output hasil build
+        emptyOutDir: true, // Menghapus isi folder sebelum build baru
+    },
+});
+
+// import { defineConfig } from 'vite'
+// import laravel from 'laravel-vite-plugin'
+
+// export default defineConfig({
+//   plugins: [
+//     laravel({
+//       input: ['resources/css/app.css', 'resources/js/app.js'],
+//       refresh: true,
+//     }),
+//   ],
+//   build: {
+//     manifest: true, // Pastikan ini true
+//     outDir: 'public/build', // Lokasi output
+//   },
+// })
