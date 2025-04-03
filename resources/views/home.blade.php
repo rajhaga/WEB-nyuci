8e97a7eebad682d6ba937ad6d9b6676212701aaa
rajhaga <134823961+rajhaga@users.noreply.github.com>
2025-04-03 17:51:00 +0700

    dist update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -132,6 +132,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <section class="text-center py-16 bg-gray-100">
